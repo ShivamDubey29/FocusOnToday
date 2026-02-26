d966a2efa168ea989666aa69fb9eb6d8ec26f633
Shivam C. Dubey <shivamc.dubey@Shivams-MacBook-Air.local>
2026-02-26 17:54:48 +0530

    Completed

--- script.js
+++ script.js
@@ -1,11 +1,44 @@
 const checkBoxlist = document.querySelectorAll('.custom-checkbox')
 const inputFields = document.querySelectorAll('.goal-input')
 const errorLabel = document.querySelector('.error-label')
+const progressLabel = document.querySelector('.progress-label')
+const cheerLabel = document.querySelector('.quote')
 const progressBar = document.querySelector('.progress-bar')
 const progressValue = document.querySelector('.progress-value')
 
+const allQuotes = ['Raise the bar by completing your goals!', 
+'Well Begun is half dine !',
+'Just a step away, keep going!',
+"Whoa ! You just completed all the goals, time for chill :D"
+ ]
 
-const allGoals = JSON.parse(localStorage.getItem('allGoals')) || {}
+ const CheerQuotes = ['Move one step ahead, today!',
+ 'Keep Going, You’re making great progress!',
+ 'You are almost their!',
+ 'Cheers You have completed today goal, Time to do it tomorrow again !'
+ ]
+
+
+const allGoals = JSON.parse(localStorage.getItem('allGoals')) || {first:{
+    name : '',
+    completed : false,
+},
+second:{
+    name : '',
+    completed : false,
+},
+third:{
+    name : '',
+    completed : false,
+},
+}
+
+let completedGoalsCount = Object.values(allGoals).filter((goal)=>goal.completed).length
+progressValue.style.width = `${completedGoalsCount / 3 * 100}%`
+progressValue.firstElementChild.innerText = `${completedGoalsCount}/3 completed`
+progressLabel.innerText = allQuotes[completedGoalsCount]
+cheerLabel.innerText = CheerQuotes[completedGoalsCount]
+
 
 checkBoxlist.forEach((checkbox)=>{
     checkbox.addEventListener('click',(e)=>{
@@ -16,10 +49,14 @@ checkBoxlist.forEach((checkbox)=>{
         
         if (allGoalsAdded){ 
         checkbox.parentElement.classList.toggle('completed')    
-        progressValue.style.width = '33.33%'    
+         
         const inputId = checkbox.nextElementSibling.id
-        console.log(inputId);
         allGoals[inputId].completed = !allGoals[inputId].completed
+        completedGoalsCount = Object.values(allGoals).filter((goal)=>goal.completed).length
+        progressValue.style.width = `${completedGoalsCount / 3 * 100}%`
+        progressValue.firstElementChild.innerText = `${completedGoalsCount}/3 completed`
+        progressLabel.innerText = allQuotes[completedGoalsCount]
+        cheerLabel.innerText = CheerQuotes[completedGoalsCount]
         localStorage.setItem('allGoals',JSON.stringify(allGoals))
         
         }else{
@@ -33,7 +70,7 @@ inputFields.forEach((input)=>{
     input.value = allGoals[input.id].name
 
     if (allGoals[input.id].completed){
-        input.parentElement.classList.add('Completed')
+        input.parentElement.classList.add('completed')
     }
 
     input.addEventListener('focus',()=>{
@@ -41,10 +78,12 @@ inputFields.forEach((input)=>{
     })
 
     input.addEventListener('input',(e)=>{
-        allGoals[input.id] = {
-            name: input.value,
-            completed: false,
+        if (allGoals[input.id].completed){
+            input.value = allGoals[input.id].name
+            return
         }
+        allGoals[input.id].name = input.value
+
         localStorage.setItem('allGoals',JSON.stringify(allGoals))
         
     })
